e5ad5195f90d152423a8602d3ae7f6ddef7a1db6
Estudiante <Estudiante@T203-13-2022>
2022-09-26 21:43:45 +0200

    Agregar archivos de proyecto.

---
+++ L5 MARH 1077822/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int variable1 = 0;
+
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("Numero Entero");
+
+variable1= Convert.ToInt32(Console.ReadLine());
+if(variable1 > 0) 
+{
+    Console.WriteLine("Numero positivo");
+}
+else if (variable1 < 0)
+{
+    Console.WriteLine("Numero negativo");
+}
+else if(variable1 == 0)
+{
+    Console.WriteLine("El numero que ingreso es cero");
+}
